101017f61a16f356fb9bf679d9c113624e2a2b9f
Bc. Martin Harman <martin.harman@student.tuke.sk>
2020-05-06 23:18:40 +0200

    numeric values changed to text(factor)

--- Main.py
+++ Main.py
@@ -1,13 +1,16 @@
 import prepare_data
 from pyspark.sql import SparkSession
+import time
 
 if __name__ == "__main__":
 
+    start = time.time()
     spark = SparkSession.builder.appName("TSVD").getOrCreate()
-
     data = prepare_data.loadData(spark)
     data = prepare_data.preprocess(data)
-    data.show()
+    prepare_data.number_to_text(data).show()
+    end = time.time()
+    print end - start
 
     spark.stop()
 
--- prepare_data.py
+++ prepare_data.py
@@ -2,7 +2,7 @@ from pyspark import SparkContext
 from pyspark.sql import Row
 from pyspark.sql.types import *
 from pyspark.sql.functions import unix_timestamp, to_date, when
-from pyspark.sql.functions import when, col , sum
+from pyspark.sql.functions import when, col, sum
 from pyspark.ml.feature import VectorAssembler
 import pyspark.sql.functions as sf
 from pyspark.sql.functions import isnan, count, avg, mean
@@ -11,11 +11,13 @@ import csv
 
 
 def loadData(spark):
-
     # CSV loading
-    df_accidents = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("./raw_data/Accidents.csv")
-    df_casualties = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("./raw_data/Casualties.csv")
-    df_vehicles = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("./raw_data/Vehicles.csv")
+    df_accidents = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(
+        "./raw_data/Accidents.csv")
+    df_casualties = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(
+        "./raw_data/Casualties.csv")
+    df_vehicles = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(
+        "./raw_data/Vehicles.csv")
 
     df_casualties = df_casualties.withColumnRenamed("Vehicle_Reference", "Vehicle_Reference_Casualty")
 
@@ -26,7 +28,6 @@ def loadData(spark):
 
 
 def preprocess(data):
-
     # time & date correction
     data = data.withColumn("Date", sf.to_date("Date", format="dd/MM/yyyy"))  # string to date
     data = data.withColumn("Date", sf.concat(sf.col('Date'), sf.lit(' '), sf.col('Time')))  # concat
@@ -38,7 +39,8 @@ def preprocess(data):
     data = reduce(lambda df, x: df.withColumn(x, to_none(x)), col_names, data)  # replace "-1" with "None"
 
     # null Location filtering
-    data = data.filter(data["Location_Easting_OSGR"].isNotNull() & data["Location_Northing_OSGR"].isNotNull() & data["Longitude"].isNotNull() & data["Latitude"].isNotNull() )
+    data = data.filter(data["Location_Easting_OSGR"].isNotNull() & data["Location_Northing_OSGR"].isNotNull() & data[
+        "Longitude"].isNotNull() & data["Latitude"].isNotNull())
 
     # NA replacement in Junction_Control => where Junction_Location = 0
     update_col = (when((col('Junction_Control').isNull()) & (col('Junction_Location') == 0), 0)
@@ -50,38 +52,29 @@ def preprocess(data):
     # other values replacement
     meanAge = data.filter(data['Age_of_Driver'].isNotNull()).agg(avg(col('Age_of_Driver'))).first()[0]
 
-    values = {'Weather_Conditions': 9, 'Journey_Purpose_of_Driver': 15, 'Sex_of_Driver': 3,
-              'Age_of_Driver': meanAge,'Age_Band_of_Driver': age_band_count(meanAge),
+    values = {'Weather_Conditions': 9,  'Sex_of_Driver': 3,
+              'Age_of_Driver': meanAge, 'Age_Band_of_Driver': age_band_count(meanAge),
               'Pedestrian_Location': 10, 'Pedestrian_Movement': 9, 'Pedestrian_Road_Maintenance_Worker': 2,
               'Junction_Control': 0}
 
     data = data.fillna(value=values)
 
     # main attr binarization
-    data = data.withColumn("Accident_Severity", when(data["Accident_Severity"] == 1, 0).otherwise(1))
-
-    data = data.withColumn('Weather_Conditions', data['Weather_Conditions'].cast(StringType()))
-    data = data.withColumn('Accident_Severity', data['Accident_Severity'].cast(StringType()))
-
-    weather = {"1": "fine", "2": "rain", "3": "snow", "4": "fine_wind", "5": "rain_wind", "6": "snow_wind", "7": "fog",
-               "8": "other", "9": "unknown"}
-    data = data.na.replace(weather, 1)
-    data = data.na.replace(["0", "1"], ['Fatal', 'NonFatal'], 'Accident_Severity')
+    data = data.withColumn("Accident_Severity_Binary", when(data["Accident_Severity"] == 1, 0).otherwise(1))
 
     # fast way
     data = toNominal(data)
 
     # 'nominal' test
-    #data.printSchema()
-
+    # data.printSchema()
 
     # temp tests
-    #data.select("Vehicle_Propulsion_Code").distinct().show()
-    #print data.filter(data["Accident_Severity"] == 1).count()
-    #print data.filter(data["Accident_Severity"] == 0).count()
+    # data.select("Vehicle_Propulsion_Code").distinct().show()
+    # print data.filter(data["Accident_Severity"] == 1).count()
+    # print data.filter(data["Accident_Severity"] == 0).count()
 
     # nul test ([tested]commented - time consuming)
-    #data.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in data.columns]).show()
+    # data.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in data.columns]).show()
 
     return data
 
@@ -105,7 +98,7 @@ def filterNa(data):
                      'Propulsion_Code', 'Age_of_Vehicle', 'Driver_IMD_Decile',
                      'Driver_Home_Area_Type',
                      'Sex_of_Casualty', 'Age_of_Casualty', 'Age_Band_of_Casualty', 'Casualty_Home_Area_Type',
-                     'LSOA_of_Accident_Location')
+                     'LSOA_of_Accident_Location', 'Journey_Purpose_of_Driver')
     return data
 
 
@@ -140,7 +133,7 @@ def toNominal(data):
     data = data.withColumn('Vehicle_Leaving_Carriageway', data['Vehicle_Leaving_Carriageway'].cast(StringType()))
     data = data.withColumn('Hit_Object_off_Carriageway', data['Hit_Object_off_Carriageway'].cast(StringType()))
     data = data.withColumn('1st_Point_of_Impact', data['1st_Point_of_Impact'].cast(StringType()))
-    data = data.withColumn('Journey_Purpose_of_Driver', data['Journey_Purpose_of_Driver'].cast(StringType()))
+    #data = data.withColumn('Journey_Purpose_of_Driver', data['Journey_Purpose_of_Driver'].cast(StringType()))
     data = data.withColumn('Sex_of_Driver', data['Sex_of_Driver'].cast(StringType()))
     data = data.withColumn('Casualty_Class', data['Casualty_Class'].cast(StringType()))
     data = data.withColumn('Casualty_Severity', data['Casualty_Severity'].cast(StringType()))
@@ -151,15 +144,10 @@ def toNominal(data):
     data = data.withColumn('Pedestrian_Road_Maintenance_Worker',
                            data['Pedestrian_Road_Maintenance_Worker'].cast(StringType()))
     data = data.withColumn('Casualty_Type', data['Casualty_Type'].cast(StringType()))
-    data = data.withColumn('Junction_Location', data['Junction_Location'].cast(StringType()))
-    data = data.withColumn('Junction_Location', data['Junction_Location'].cast(StringType()))
-    data = data.withColumn('Junction_Location', data['Junction_Location'].cast(StringType()))
-    data = data.withColumn('Junction_Location', data['Junction_Location'].cast(StringType()))
-    data = data.withColumn('Junction_Location', data['Junction_Location'].cast(StringType()))
-    data = data.withColumn('Junction_Location', data['Junction_Location'].cast(StringType()))
-    data = data.withColumn('Junction_Location', data['Junction_Location'].cast(StringType()))
-    data = data.withColumn('Junction_Location', data['Junction_Location'].cast(StringType()))
-    data = data.withColumn('Junction_Location', data['Junction_Location'].cast(StringType()))
+    data = data.withColumn('Weather_Conditions', data['Weather_Conditions'].cast(StringType()))
+    data = data.withColumn('Accident_Severity', data['Accident_Severity'].cast(StringType()))
+    data = data.withColumn('Age_Band_of_Driver', data['Age_Band_of_Driver'].cast(StringType()))
+
     return data
 
 
@@ -191,3 +179,129 @@ def age_band_count(age):
 
 def to_none(col_name):
     return when(col(col_name) != "-1", col(col_name)).otherwise(None)
+
+
+def number_to_text(data):
+    data = data.replace(["1", "2", "3"], ["fatal", "serious", "slight"], "Accident_Severity")
+
+    weather = {"1": "fine", "2": "rain", "3": "snow", "4": "fine-wind", "5": "rain-wind", "6": "snow-wind", "7": "fog",
+               "8": "other", "9": "unknown"}
+    day = {"1": "sunday", "2": "monday", "3": "tuesday", "4": "wednesday", "5": "thursday", "6": "friday",
+           "7": "saturday"}
+    road_class1 = {"1": "motorway", "2": "A(M)", "3": "A", "4": "B", "5": "C", "6": "unclassified"}
+    road_class2 = {"0": "not-junction", "1": "motorway", "2": "A(M)", "3": "A", "4": "B", "5": "C", "6": "unclassified"}
+    road_type = {"1": "roundabout", "2": "one-way-street", "3": "dual-carriageway", "6": "single-carriageway",
+                 "7": "slip-road", "9": "unknown", "12": "one-way/slip-road"}
+    light = {"1": "daylight", "4": "darkness-lit", "5": "darkness-unlit", "6": "darkness-unknown"}
+    junction = {"0": "not-junction", "1": "roundabout", "2": "mini-roundabout", "3": "t/staggered-junction",
+                "5": "slip-road", "6": "crossroads", "7": "more-than-4-arms", "8": "private-drive", "9": "other"}
+    junction_control = {"0": "not-junction", "1": "authorised-person", "2": "auto-traffic-signal", "3": "stop-sign",
+                        "4": "give-way/uncontrolled"}
+    ped_cross_human = {"0": "none", "1": "school-crossing-patrol", "2": "authorised-person"}
+    ped_cross_physical = {"0": "no-facilities", "1": "zebra", "4": "pedestrian-light-controlled-crossing",
+                          "5": "pedestrian-light", "7": "footbridge/subway", "8": "central-refuge"}
+    road_surface = {"1": "dry", "2": "wet", "3": "snow", "4": "ice", "5": "flood", "6": "oil", "7": "mud"}
+    spec_conditions = {"0": "none", "1": "auto-signal-out", "2": "auto-signal-defective", "3": "road-sign",
+                       "4": "roadworks", "5": "road-surface-defective", "6": "oil", "7": "mud"}
+    hazards = {"0": "none", "1": "vehicle", "2": "object", "3": "accident", "4": "dog",
+               "5": "pedestrian", "7": "animal"}
+    area = {"1": "urban", "2": "rural", "3": "unallocated"}
+    police_attend = {"1": "yes", "2": "no", "3": "no"}
+    # motorcycle a goods som spojil dokopy
+    vehicle_type = {"1": "pedal-cycle", "2": "motorcycle", "3": "motorcycle", "4": "motorcycle", "5": "motorcycle",
+                    "8": "taxi", "9": "car", "10": "minibus", "11": "bus", "16": "horse", "17": "agricultural-vehicle",
+                    "18": "tram", "19": "van", "20": "goods", "21": "goods", "22": "scooter", "23": "motorcycle",
+                    "90": "other", "97": "motorcycle", "98": "goods"}
+    sex = {"1": "male", "2": "female", "3": "not-known"}
+    # ak by niekoho napadly lepsie nazvy cca podla toho http://www.fsps.muni.cz/emuni/data/reader/book-19/04.html
+    age_band = {"1": "child", "2": "school-child", "3": "teen", "4": "child", "5": "young-adult", "6": "adult",
+                "7": "middle-adult", "8": "old-adult", "9": "young-senior", "10": "middle-senior", "11": "old-senior"}
+    impact = {"0": "no-impact", "1": "front", "2": "back", "3": "offside", "4": "nearside"}
+    hit_off_carriageway = {"0": "none", "1": "sign", "2": "lamp", "3": "pole", "4": "tree", "5": "bus-stop",
+                           "6": "central-crash-barrier", "7": "near/offside-barrier", "8": "submerged",
+                           "9": "entered-ditch", "10": "other-object", "11": "wall"}
+    leaving_carriageway = {"0": "didnt-leave", "1": "nearside", "2": "nearside-rebound", "3": "straight-ahead",
+                           "4": "offside-central", "5": "offside-central-rebound", "6": "offside-cross-central",
+                           "7": "offside", "8": "offside-rebound"}
+    hit_in_carriageway = {"0": "none", "1": "previous-accident", "2": "road-works", "4": "parked-vehicle",
+                          "5": "bridge(roof)", "6": "bridge(side)", "7": "bollard-of-refuge", "8": "open-door-of-vehicle",
+                          "9": "central-island-of-roundabout", "10": "kerb", "11": "other-object", "12": "any-animal"}
+    skidding = {"0": "none", "1": "skidded", "2": "skidded-overturned", "3": "jackknifed", "4": "jackknifed-overturned",
+                "5": "overturned"}
+    junction_location = {"0": "not-junction", "1": "approaching-junction", "2": "cleared-junction",
+                         "3": "leaving-roundabout", "4": "entering-roundabout", "5": "leaving-main-road",
+                         "6": "entering-main-road", "7": "entering-from-slip-road", "8": "mid-junction"}
+    vehicle_location = {"0": "on-main", "1": 'tram-track', "2": "bus-lane", "3": "busway", "4": "cycleway",
+                        "5": "cycleway/footway", "6": "on-lay-by", "7": "entering-lay-by", "8": "leaving-lay-by",
+                        "9": "footway", "10": "not-on-carriageway"}
+    vehicle_manoeuvre = {"1": "reversing", "2": "parked", "3": "w8-to-go", "4": "slowing/stopping", "5": "moving-off",
+                         "6": "u-turn", "7": "turning-left", "8": "w8-to-turn-left", "9": "turning-right",
+                         "10": "w8-to-turn-right", "11": "changing-lane-to-left", "12": "changing-lane-to-right",
+                         "13": "overtaking-moving-vehicle", "14": "overtaking-static-vehicle",
+                         "15": "overtaking-nearside", "16": "going-ahead-left-hand-bend",
+                         "17": "going-ahead-right-hand-bend", "18": "going-ahead-other"}
+    towing = {"0": "no-tow", "1": "articulated", "2": "multiple-trailer", "3": "caravan", "4": "single-trailer",
+              "5": "other-tow"}
+    casualty_class = {"1": "driver", "2": "passenger", "3": "pedestrian"}
+    casualty_severity = {"1": "fatal", "2": "serious", "3": "slight"}
+    ped_location = {"0": "not-pedestrian", "1": "crossing-on-pedestrian-crossing-facility",
+                    "2": "crossing-in-zig-zag-approach-lines", "3": "crossing-in-zig-zag-exit-lines",
+                    "4": "crossing-elsewhere", "5": "in-carriageway-crossing", "6": "on-footway", "7": "on-refuge",
+                    "8": "in-centre-of-carriageway", "9": "in-carriageway-not-crossing", "10": "unknown"}
+    ped_movement = {"0": "not-pedestrian", "1": "crossing-from-driver-nearside", "2": "crossing-from-nearside-masked",
+                    "3": "crossing-from-driver-offside", "4": "crossing-from-offside-masked",
+                    "5": "in-carriageway-stationary-not-crossing", "6": "in-carriageway-stationary-not-crossing-masked",
+                    "7": "walking-along-in-carriageway-facing-traffic",
+                    "8": "walking-along-in-carriageway-back-to-traffic", "9": "unknown"}
+    car_passenger = {"0": "not-passenger", "1": "font-seat", "2": "rear-seat"}
+    bus_passenger = {"0": "not-passenger", "1": "boarding", "2": "alighting", "3": "standing-passenger",
+                     "4": "seated-passenger"}
+    maintenance_worker = {"0": "no", "1": "yes", "2": "unknown"}
+    casualty_type = {"0": "pedestran", "1": "cyclist", "2": "motorcycler", "3": "motorcycler", "4": "motorcycler",
+                     "5": "motorcycler", "8": "taxi-occupant", "9": "car-occupant", " 10": "minibus-occupant",
+                     "11": "bus-occupant", "16": "horse-rider", "17": "agricultural-occupant", "18": "tram-occupant",
+                     "19": "van-occupant", "20": "goods-occupant", "21": "goods-occupant", "22": "scooter-rider",
+                     "23": "motocycler", "90": "other-occupant", "97": "motorcycler", "98": "goods-occupant"}
+
+    data = data.replace(to_replace=weather, subset="Weather_Conditions")
+    data = data.replace(to_replace=day, subset="Day_of_Week")
+    data = data.replace(to_replace=road_class1, subset="1st_Road_class")
+    data = data.replace(to_replace=road_class2, subset="2st_Road_class")
+    data = data.replace(to_replace=road_type, subset="Road_Type")
+    data = data.replace(to_replace=light, subset="Light_Conditions")
+    data = data.replace(to_replace=junction, subset="Junction_Detail")
+    data = data.replace(to_replace=junction_control, subset="Junction_Control")
+    data = data.replace(to_replace=ped_cross_human, subset="Pedestrian_Crossing-Human_Control")
+    data = data.replace(to_replace=ped_cross_physical, subset="Pedestrian_Crossing-Physical_Facilities")
+    data = data.replace(to_replace=road_surface, subset="Road_Surface_Conditions")
+    data = data.replace(to_replace=spec_conditions, subset="Special_Conditions_at_Site")
+    data = data.replace(to_replace=hazards, subset="Carriageway_Hazards")
+    data = data.replace(to_replace=area, subset="Urban_or_Rural_Area")
+    data = data.replace(to_replace=police_attend, subset="Did_Police_Officer_Attend_Scene_of_Accident")
+    data = data.replace(to_replace=vehicle_type, subset="Vehicle_Type")
+    data = data.replace(to_replace=sex, subset="Sex_of_Driver")
+    data = data.replace(to_replace=age_band, subset="Age_Band_of_Driver")
+    data = data.replace(to_replace=impact, subset="1st_Point_of_Impact")
+    data = data.replace(to_replace=hit_off_carriageway, subset="Hit_Object_off_Carriageway")
+    data = data.replace(to_replace=leaving_carriageway, subset="Vehicle_Leaving_Carriageway")
+    data = data.replace(to_replace=hit_in_carriageway, subset="Hit_Object_in_Carriageway")
+    data = data.replace(to_replace=skidding, subset="Skidding_and_Overturning")
+    data = data.replace(to_replace=junction_location, subset="Junction_Location")
+    data = data.replace(to_replace=vehicle_location, subset="Vehicle_Location-Restricted_Lane")
+    data = data.replace(to_replace=vehicle_manoeuvre, subset="Vehicle_Manoeuvre")
+    data = data.replace(to_replace=towing, subset="Towing_and_Articulation")
+    data = data.replace(to_replace=casualty_class, subset="Casualty_Class")
+    data = data.replace(to_replace=casualty_severity, subset="Casualty_Severity")
+    data = data.replace(to_replace=ped_location, subset="Pedestrian_Location")
+    data = data.replace(to_replace=ped_movement, subset="Pedestrian_Movement")
+    data = data.replace(to_replace=car_passenger, subset="Car_Passenger")
+    data = data.replace(to_replace=bus_passenger, subset="Bus_or_Coach_Passenger")
+    data = data.replace(to_replace=maintenance_worker, subset="Pedestrian_Road_Maintenance_Worker")
+    data = data.replace(to_replace=casualty_type, subset="Casualty_Type")
+
+    return data
+
+
+def make_histograms(data):
+    # TODO create histograms for nominal atritubtes
+    return None
